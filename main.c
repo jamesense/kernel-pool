768c6bf6f6c49d7cfb7174180f9522ac737cd3dc
jamesense <dscjkbiaacascyha@gmail.com>
2025-02-19 10:01:52 +0000

    Update main.c - added management of pools

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <ntifs.h>
 
-void AllocPool(SIZE_T allocSize) {
-	PVOID pool = ExAllocatePoolWithTag(POOL_RAISE_IF_ALLOCATION_FAILURE, allocSize, 'Drvr');
-	if (pool)
-	{
-		DbgPrint("Pool has been successfully made");
-
-		ExFreePoolWithTag(pool, 'Drvr');
+PVOID AllocPool(SIZE_T allocSize, ULONG Tag) {
+	PVOID pool = ExAllocatePoolWithTag(NonPagedPool, allocSize, Tag);
+	if (pool) {
+		DbgPrint("Pool has been successfully allocated at %p", pool);
 	}
-	else
-	{
-		DbgPrint("Pool has not been created");
+	else {
+		DbgPrint("Pool allocation failed");
+	}
+	return pool;
+}
+
+void FreePool(PVOID pool, ULONG Tag) {
+	if (pool) {
+		DbgPrint("Freeing pool at %p", pool);
+		ExFreePoolWithTag(pool, Tag);
 	}
 }
 
@@ -21,7 +25,16 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
 
-	AllocPool(0x1024);
+	PVOID pool0001 = AllocPool(0x1024, '0001');
+	if (pool0001)
+	{
+		DbgPrint("Hello from pool!!!");
+		strcpy((char*)pool0001, "Hello dudes!!");
+
+		DbgPrint("%s", (char*)pool0001); //prints out the string that we passed into the pool
+
+		FreePool(pool0001, '0001');
+	}
 
 	return STATUS_SUCCESS;
 }
